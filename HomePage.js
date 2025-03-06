9e696c736e773abaf573c9b84004da05f1daf94a
ipewzner <ipewzner@gmail.com>
2025-03-06 20:10:59 +0200

    Correcting spelling errors 2

--- HomePage.js
+++ HomePage.js
@@ -6,7 +6,7 @@ class HomePage extends HTMLElement { // Changed class name to DemosPage
 
             <p>The multidisciplinary revolution in science did not skip over marine biology and engineering. Today, we understand marine ecosystems in ways that were previously unimaginable. Tools such as remote sensing, satellite imaging, and aerial and marine robotics allow scientists to study vast and often inaccessible areas of the ocean and obtain detailed data on biodiversity, species behavior and environmental changes.</p>
             <br> 
-            <p> These technologies also help with real-time monitoring, such as detecting pollutants or tracking the eKects of climate change on the marine environment. By harnessing these tools, marine biologists can make more informed conservation decisions, protect endangered species and better manage marine resources for the future.</p>
+            <p> These technologies also help with real-time monitoring, such as detecting pollutants or tracking the Effects of climate change on the marine environment. By harnessing these tools, marine biologists can make more informed conservation decisions, protect endangered species and better manage marine resources for the future.</p>
             <br> 
             <p>The 2025 Haifa Tech4Bio Workshop will bring together experts, students and decision makers to showcase the achievements of combining technology and marine biology and to explore the challenges ahead. The Tech4Bio workshop will be a two-day event comprising of five units:</p>
 
